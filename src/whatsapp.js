d176113425c626d2ca7e3841c697fcb27007e4b1
Jeisson Benavides <projektjb99@gmail.com>
2025-08-13 11:49:52 -0500

    cambie la palabra

--- src/whatsapp.js
+++ src/whatsapp.js
@@ -28,7 +28,7 @@ async function enviarOpcionesIniciales(to) {
     type: "interactive",
     interactive: {
       type: "list",
-      header: { type: "text", text: "Bienvenido 👋" },
+      header: { type: "text", text: "alo 👋" },
       body: { text: "Selecciona una opción para continuar:" },
       footer: { text: "Bot" },
       action: {
